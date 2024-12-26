bd3bd8d9b34f22651d3081d3a6b1cbe53bdab45a
Kidō <chikyu.kido@posteo.de>
2024-12-26 18:53:46 +0100

    added templates

--- wat/util/serve_file.go
+++ wat/util/serve_file.go
@@ -2,8 +2,11 @@ package wat
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/andybalholm/brotli"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"html/template"
 	"io/fs"
 	"mime"
 	"os"
@@ -15,7 +18,16 @@ import (
 var (
 	cache      = make(map[string][]byte)
 	cacheMutex = &sync.Mutex{}
+	templates  []string
 )
+
+func unescapeJavaScript(content []byte) []byte {
+	strContent := string(content)
+	strContent = strings.ReplaceAll(strContent, "&lt;", "<")
+	strContent = strings.ReplaceAll(strContent, "&gt;", ">")
+	strContent = strings.ReplaceAll(strContent, "&amp;", "&")
+	return []byte(strContent)
+}
 
 func getCachedContent(path string, filepath string) []byte {
 	cacheMutex.Lock()
@@ -24,7 +36,25 @@ func getCachedContent(path string, filepath string) []byte {
 		return content
 	}
 	var content []byte
-	content, _ = os.ReadFile(filepath)
+	if strings.HasSuffix(filepath, ".html") || strings.HasSuffix(filepath, ".js") {
+		byteBuffer := bytes.NewBuffer(make([]byte, 0))
+		t, err := template.ParseFiles(append([]string{filepath}, templates...)...)
+		if err != nil {
+			fmt.Printf("Failed to parse template %v", err)
+			return nil
+		}
+		err = t.Execute(byteBuffer, nil)
+		if err != nil {
+			fmt.Printf("Failed to execute template %v", err)
+			return nil
+		}
+		content = byteBuffer.Bytes()
+		if strings.HasSuffix(filepath, ".js") {
+			content = unescapeJavaScript(content)
+		}
+	} else {
+		content, _ = os.ReadFile(filepath)
+	}
 	if !strings.Contains(filepath, "imgs") {
 		var compressedContent bytes.Buffer
 		compressionLevel := brotli.BestSpeed
@@ -43,6 +73,22 @@ func getCachedContent(path string, filepath string) []byte {
 		cache[path] = content
 	}
 	return cache[path]
+}
+func LoadTemplates(directory string) {
+	var files []string
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(info.Name()) == ".html" {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		logrus.Fatalf("failed to load templates")
+	}
+	templates = files
 }
 
 func ServeFile(relPath, path string, r *gin.RouterGroup) {
